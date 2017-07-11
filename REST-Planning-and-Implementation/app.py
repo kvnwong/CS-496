dd5386de746ff43dc04d9718961b6846a5eb14df
Jon-Eric Cook <mrjec@protonmail.com>
2017-07-11 14:48:30 -0700

    keep on working

--- REST-Planning-and-Implementation/app.py
+++ REST-Planning-and-Implementation/app.py
@@ -1,7 +1,6 @@
 from google.appengine.ext import ndb
 import webapp2
 import json
-import types
 
 class Boat(ndb.Model):
     id = ndb.StringProperty()
@@ -12,14 +11,14 @@ class Boat(ndb.Model):
 
 class BoatHandler(webapp2.RequestHandler):
     def post(self):
-        post_boat_data = json.loads(self.request.body)
-        post_boat = Boat(name=post_boat_data['name'],
-                        type=post_boat_data['type'],
-                        length=post_boat_data['length'],
-                        at_sea=post_boat_data['at_sea'])
+        post_data = json.loads(self.request.body)
+        post_boat = Boat(name=post_data['name'],
+                    type=post_data['type'],
+                    length=post_data['length'],
+                    at_sea=post_data['at_sea'])
 
-        if post_boat.at_sea:
-            post_boat.at_sea = false
+        if not post_boat.at_sea:
+            post_boat.at_sea = True
 
         post_boat.put()
         post_boat.id = str(post_boat.key.urlsafe())
@@ -31,24 +30,25 @@ class BoatHandler(webapp2.RequestHandler):
     def get(self, id=None):
         if id:
             get_boat = ndb.Key(urlsafe=id).get()
-            get_boat_dict= get_boat.to_dict()
+            get_boat_dict = get_boat.to_dict()
             get_boat_dict['self'] = "/boats/" + id
             self.response.write(json.dumps(get_boat_dict))
         else:
             get_boat_query_results = [get_boat_query.to_dict()
                                       for get_boat_query in Boat.query()]
 
-            for boat in get_boat_query_results:
-                boat['self'] = "/boats/" + boat['id']
+            for single_boat in get_boat_query_results:
+                single_boat['self'] = "/boats/" + str(single_boat['id'])
 
             self.response.write(json.dumps(get_boat_query_results))
 
     def delete(self, id=None):
         if id:
-            slip_boat_is_in = Slip.query(Slip.current_boat == id)
-            slip_boat_is_in.current_boat = ""
-            slip_boat_is_in.arrival_date = ""
-            slip_boat_is_in.put()
+            for slip in Slip.query(Slip.current_boat == id):
+                slip.current_boat = ""
+                slip.arrival_date = ""
+                slip.put()
+
             ndb.Key(urlsafe=id).delete()
             self.response.write("boat was deleted")
 
@@ -58,7 +58,7 @@ class BoatHandler(webapp2.RequestHandler):
             patch_boat = ndb.Key(urlsafe=id).get()
 
             if len(patch_boat_data) > 1:
-                self.response.write("can only change one item")
+                self.response.write("can only change one item per PATCH")
             else:
                 for key in patch_boat_data:
                     if key == "name":
@@ -71,21 +71,31 @@ class BoatHandler(webapp2.RequestHandler):
                         patch_boat.length = patch_boat_data['length']
                         self.response.write("boat length was updated")
                     else:
-                        self.response.write("boat name, type and length can only be edited by a PATCH")
+                        self.response.write("boat name, type and length can only be edited by via PATCH")
                 patch_boat.put()
 
     def put(self, id=None):
         if id:
             put_boat_data = json.loads(self.request.body)
-            if len(put_boat_data) < 3:
-                self.response.write("name, type and length should be in body")
-            else:
-                put_boat = ndb.Key(urlsafe=id).get()
+            put_boat = ndb.Key(urlsafe=id).get()
+            input_name = False
+            input_type = False
+            input_length = False
+            for item in put_boat_data:
+                if item == "name":
+                    input_name = True
+                elif item == "type":
+                    input_type = True
+                elif item == "length":
+                    input_length = True
+            if input_name and input_type and input_length:
                 put_boat.name = put_boat_data['name']
                 put_boat.type = put_boat_data['type']
                 put_boat.length = put_boat_data['length']
                 put_boat.put()
                 self.response.write("boat was updated")
+            else:
+                self.response.write("bad data: name, type and length should be in PUT body")
 
 class Slip(ndb.Model):
     id = ndb.StringProperty()
@@ -124,7 +134,7 @@ class SlipHandler(webapp2.RequestHandler):
     def get(self, id=None):
         if id:
             get_slip = ndb.Key(urlsafe=id).get()
-            get_slip_dict= get_slip.to_dict()
+            get_slip_dict = get_slip.to_dict()
             get_slip_dict['self'] = "/slips/" + id
             self.response.write(json.dumps(get_slip_dict))
         else:
@@ -165,8 +175,64 @@ class SlipHandler(webapp2.RequestHandler):
                         if not found_it:
                             patch_slip.number = patch_slip_data['number']
                             self.response.write("slip number was updated")
+                    else:
+                        self.response.write("can only upate 'number' via PATCH")
 
                 patch_slip.put()
+
+class BoatInSlipHandler(webapp2.RequestHandler):
+    def get(self, id=None):
+        if id:
+            get_slip = ndb.Key(urlsafe=id).get()
+            if get_slip.current_boat == "":
+                self.response.write("no boat in slip")
+            else:
+                get_boat = ndb.Key(urlsafe=get_slip.current_boat).get()
+                boat_dict = get_boat.to_dict()
+                boat_dict['self'] = "/boats/" + get_slip.current_boat
+                self.response.write(json.dumps(boat_dict))
+
+    def put(self, id=None):
+        if id:
+            put_data = json.loads(self.request.body)
+            put_boat = ndb.Key(urlsafe=put_data['current_boat']).get()
+            if put_boat.at_sea:
+                input_current_boat = False
+                input_arrival_date = False
+                for item in put_data:
+                    if item == "current_boat":
+                        input_current_boat = True
+                    elif item == "arrival_date":
+                        input_arrival_date = True
+                if input_current_boat and input_arrival_date:
+                    put_slip = ndb.Key(urlsafe=id).get()
+                    if put_slip.current_boat == "":
+                        put_slip.current_boat = put_data['current_boat']
+                        put_slip.arrival_date = put_data['arrival_date']
+                        put_slip.put()
+                        put_boat.at_sea = False
+                        put_boat.put()
+                        self.response.write("boat was added to slip")
+                    else:
+                        self.response.write("slip is already occupied")
+                else:
+                    self.response.write("bad data: current_boat and arrival_date should be in PUT body")
+            else:
+                self.response.write("selected boat is already in a slip")
+
+    def delete(self, id=None):
+        if id:
+            slip = ndb.Key(urlsafe=id).get()
+            if slip.current_boat != "":
+                boat_in_slip = ndb.Key(urlsafe=slip.current_boat).get()
+                boat_in_slip.at_sea = True
+                boat_in_slip.put()
+                slip.current_boat = ""
+                slip.arrival_date = ""
+                slip.put()
+                self.response.write("boat was removed from slip")
+            else:
+                self.response.write("no boat in slip")
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -181,6 +247,6 @@ app = webapp2.WSGIApplication([
     ('/boats',BoatHandler),
     ('/boats/(.*)',BoatHandler),
     ('/slips',SlipHandler),
-    #('/slips/(.*)/boat',BoatInSlipHandler),
+    ('/slips/(.*)/boat',BoatInSlipHandler),
     ('/slips/(.*)',SlipHandler)
 ], debug=True)
