316d72ba176c9693ef0950d9677b20148bb0c354
Jon-Eric Cook <mrjec@protonmail.com>
2017-07-18 23:00:22 -0700

    done

--- OAuth-2.0-Implementation/main.py
+++ OAuth-2.0-Implementation/main.py
@@ -1,40 +1,97 @@
+from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
+import webapp2
+import urllib
+import random
+import string
 import json
-import flask
-import requests
-
-app = flask.Flask(__name__)
+import os
 
 CLIENT_ID = '913425740949-0c0i284ks9elrosgpv5rdh2qvsal8cim.apps.googleusercontent.com'
 CLIENT_SECRET = 'tKkgsZh6HK2zWcNeOrlE9kH3'
-REDIRECT_URI = 'http://localhost:8080/oauth'
+REDIRECT_URI = 'https://oauth2-imp.appspot.com/oauth'
 
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        random_string = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
 
-@app.route('/oauth')
-def oauth():
-    auth_code = flask.request.args.get('code')
-    data = {'code': auth_code,
+        url_linktext = 'Provide Access'
+
+        url = "https://accounts.google.com/o/oauth2/v2/auth?"
+        url = url + "scope=email"
+        url = url + "&access_type=offline"
+        url = url + "&include_granted_scopes=true"
+        url = url + "&state="
+        url = url + random_string
+        url = url + "&redirect_uri=https://oauth2-imp.appspot.com/oauth"
+        url = url + "&response_type=code"
+        url = url + "&client_id=913425740949-0c0i284ks9elrosgpv5rdh2qvsal8cim.apps.googleusercontent.com"
+
+        template_values = {'url': url}
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        self.response.out.write(template.render(path, template_values))
+
+class OAuthHandler(webapp2.RequestHandler):
+    def get(self):
+        auth_code = self.request.GET['code']
+        state = self.request.GET['state']
+        post_body = {
+            'code': auth_code,
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET,
             'redirect_uri': REDIRECT_URI,
-            'grant_type': 'authorization_code'}
+            'grant_type': 'authorization_code'
+            }
 
-    r = requests.post('https://www.googleapis.com/oauth2/v4/token', data=data)
-    credendtials = json.loads(r.text)
+        payload = urllib.urlencode(post_body)
+        headers = {'Content-Type':'application/x-www-form-urlencoded'}
+        result = urlfetch.fetch(
+            url="https://www.googleapis.com/oauth2/v4/token",
+   		 	payload = payload,
+    		method = urlfetch.POST,
+    		headers = headers)
 
-    headers = {'Authorization': 'Bearer ' + credendtials['access_token']}
-    req_uri = 'https://www.googleapis.com/plus/v1/people/me'
-    r = requests.get(req_uri, headers=headers)
-    content = json.loads(r.text)
+        json_result = json.loads(result.content)
 
-    fname = content.name['givenName']
-    lname = content.name['familyName']
-    gplink = content.url
+        headers = {'Authorization': 'Bearer ' + json_result['access_token']}
+        result = urlfetch.fetch(
+            url="https://www.googleapis.com/plus/v1/people/me",
+            method = urlfetch.GET,
+            headers=headers)
 
-    return flask.render_template('oauth.html',
-            fname=fname,
-            lname=lname,
-            gplink=gplink,
-            state=CLIENT_SECRET)
+        json_result = json.loads(result.content)
+        # self.response.write(json.dumps(json_result['name']['givenName']))
+        exist_fname = False
+        exist_lname = False
+        exist_gplink = False
+        exist_account = False
+        for item in json_result:
+            if item == 'name':
+                if item[0]:
+                    exist_fname = True
+                if item[0]:
+                    exist_lname = True
+            if item == 'url':
+                exist_gplink = True
+
+        if exist_fname and exist_lname and exist_gplink:
+            fname = json_result['name']['givenName']
+            lname = json_result['name']['familyName']
+            gplink = str(json_result['url'])
+            exist_account = True
+            template_values = {'fname': fname,
+                               'lname': lname,
+                               'gplink': gplink,
+                               'gplink_name': "Visit Profile",
+                               'state': state}
+        else:
+            template_values = {'noAccount': "NOTE: No Google+ account found", 'state': state}
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/oauth.html')
+        self.response.out.write(template.render(path, template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/oauth',OAuthHandler)
+], debug=True)
